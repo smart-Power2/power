bf960c1f03d9953d564d975fd8ed78586673fa2a
Mohamed-Khalil-Dhaher <dhaherkhalil91@gmail.com>
2021-06-23 15:42:48 +0100

    search bar design

--- front-end/src/app/app.routing.ts
+++ front-end/src/app/app.routing.ts
@@ -1,33 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
 
-import { ComponentsComponent } from './components/components.component';
-import { LandingComponent } from './examples/landing/landing.component';
-import { LoginComponent } from './examples/login/login.component';
-import { ProfileComponent } from './examples/profile/profile.component';
-import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
-import { SignupComponent } from './signup/signup.component';
-import {SearchComponent} from './search/search.component'
+import { ComponentsComponent } from "./components/components.component";
+import { LandingComponent } from "./examples/landing/landing.component";
+import { LoginComponent } from "./examples/login/login.component";
+import { ProfileComponent } from "./examples/profile/profile.component";
+import { NucleoiconsComponent } from "./components/nucleoicons/nucleoicons.component";
+import { SignupComponent } from "./signup/signup.component";
+import { SearchComponent } from "./search/search.component";
 
-const routes: Routes =[
-    { path: '', redirectTo: 'index', pathMatch: 'full' },
-    { path: 'search',                component: SearchComponent },
-    { path: 'signup',                component: SignupComponent },
-    { path: 'nucleoicons',          component: NucleoiconsComponent },
-    { path: 'examples/landing',     component: LandingComponent },
-    { path: 'examples/login',       component: LoginComponent },
-    { path: 'examples/profile',     component: ProfileComponent }
+const routes: Routes = [
+  { path: "", redirectTo: "index", pathMatch: "full" },
+  { path: "index", component: ComponentsComponent },
+  { path: "search", component: SearchComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "nucleoicons", component: NucleoiconsComponent },
+  { path: "examples/landing", component: LandingComponent },
+  { path: "examples/login", component: LoginComponent },
+  { path: "examples/profile", component: ProfileComponent }
 ];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        BrowserModule,
-        RouterModule.forRoot(routes)
-    ],
-    exports: [
-    ],
+  imports: [CommonModule, BrowserModule, RouterModule.forRoot(routes)],
+  exports: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
