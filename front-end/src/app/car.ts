b589d1909435ec758b14dd922a39e0ef82be562f
malekhizaoui <hizaoui.malek.habib@gmail.com>
2021-06-24 11:27:38 +0100

    refactor all my work from scratch

---
+++ front-end/src/app/car.ts
@@ -0,0 +1,14 @@
+export interface Car {
+    brand: string;
+    model: string;
+    engine: string;
+    price: number;
+    seats: number;
+    doors: number;
+    suitcases: number;
+    airconditioner: string;
+    transmission: string;
+    file1: string;
+    file2: string;
+    file3: string;
+  }
